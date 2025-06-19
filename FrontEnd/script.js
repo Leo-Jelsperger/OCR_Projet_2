389caa427fba0be7c92c58953d845b86ad82c5ce
Leo-Jelsperger <158077569+Leo-Jelsperger@users.noreply.github.com>
2025-06-19 17:20:52 +0200

    feat: add article filter buttons

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -13,13 +13,14 @@ async function getData() {
   }
 }
 
+const gallery = document.querySelector(".gallery");
+
 getData().then((data) => {
-  const gallery = document.querySelector(".gallery");
   let html = "";
   console.log(data);
   data.forEach((elt) => {
     html += `
-    <figure>
+    <figure class="category-${elt.categoryId}">
       <img
         src="${elt.imageUrl}"
         alt="${elt.title}"
@@ -28,4 +29,42 @@ getData().then((data) => {
     </figure>`;
   });
   gallery.innerHTML = html;
+});
+
+const filters = [
+  { name: "allFilter", id: "#all", categoryId: 0 },
+  { name: "itemFilter", id: "#items", categoryId: 1 },
+  { name: "flatFilter", id: "#flats", categoryId: 2 },
+  {
+    name: "hotelsFilter",
+    id: "#hotels-and-restaurants",
+    categoryId: 3,
+  },
+];
+
+function filterClick(targetedClass, activeBtn) {
+  const figures = gallery.querySelectorAll("figure");
+  const btn = document.querySelectorAll(".filter-buttons");
+  console.log(activeBtn);
+  btn.forEach((elt) => elt.classList.remove("used-btn"));
+  activeBtn.classList.add("used-btn");
+  if (targetedClass === "category-0") {
+    figures.forEach((elt) => {
+      elt.classList.remove("display-none");
+    });
+  } else {
+    figures.forEach((elt) => elt.classList.add("display-none"));
+    gallery
+      .querySelectorAll(`.${targetedClass}`)
+      .forEach((elt) => elt.classList.remove("display-none"));
+  }
+}
+
+filters.forEach((filter) => {
+  const element = document.querySelector(filter.id);
+  if (element) {
+    element.addEventListener("click", () =>
+      filterClick(`category-${filter.categoryId}`, element)
+    );
+  }
 });
