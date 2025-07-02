ac63d19879e60be4108aa70fc4364ad6509513d4
Leo-Jelsperger <158077569+Leo-Jelsperger@users.noreply.github.com>
2025-07-02 14:28:32 +0200

    feat: articles can now be deleted

--- FrontEnd/script-index.js
+++ FrontEnd/script-index.js
@@ -39,7 +39,7 @@ getData().then((data) => {
   data.forEach((elt) => {
     const temp = document.createElement("div");
     temp.innerHTML = `
-      <figure data-category-id="${elt.categoryId}">
+      <figure data-id="${elt.id}" data-category-id="${elt.categoryId}">
         <img src="${elt.imageUrl}" alt="${elt.title}" />
         <figcaption>${elt.title}</figcaption>
       </figure>
@@ -75,50 +75,125 @@ function createBtnEvent() {
 
 createBtnEvent();
 
-function openModale() {
+async function openModale() {
   const temp = document.createElement("div");
   temp.innerHTML = `
-   <section id="modale">
+  <div id="modale-window">
+    <div id="modale-wrapper">
       <button id="close">
         <i class="fa-solid fa-xmark"></i>
       </button>
-      <h2>Galerie photo</h2>
-      <div id="modale-gallery"></div>
-      <button id="add">Ajouter une photo</button>
-    </section>
+      <button id="back">
+        <i id="back" class="fa-solid fa-arrow-left"></i>
+      </button>  
+      <section id="modale-main">
+        <h2>Galerie photo</h2>
+        <div id="modale-gallery"></div>
+        <button id="add">Ajouter une photo</button>
+      </section>
+      <section id="modale-form">
+        <h2>Ajout photo</h2>
+      </section>
+    </div>
+  </div>
   `;
+
   const bgTemp = document.createElement("div");
   bgTemp.innerHTML = `  
     <div id="modale-bg"></div>
     `;
+
   const modaleWindow = temp.firstElementChild;
   const modaleBg = bgTemp.firstElementChild;
-  document.querySelector("body").appendChild(modaleWindow);
-  document.querySelector("body").appendChild(modaleBg);
-  const closeBtn = document.querySelector("#close");
-  closeBtn.addEventListener("click", closeModale);
-  document.querySelector("body").style.overflow = "hidden";
-  const modaleGallery = document.querySelector("#modale-gallery");
 
-  getData().then((data) => {
-    data.forEach((elt) => {
-      const temp = document.createElement("div");
-      temp.innerHTML = `
-      <figure data-category-id="${elt.categoryId}">
+  document.body.appendChild(modaleWindow);
+  document.body.appendChild(modaleBg);
+  document.body.style.overflow = "hidden";
+
+  modaleWindow.querySelector("#close").addEventListener("click", closeModale);
+  modaleWindow.querySelector("#add").addEventListener("click", showAddForm);
+
+  modaleBg.addEventListener("click", closeModale);
+
+  const modaleGallery = modaleWindow.querySelector("#modale-gallery");
+  const data = await getData();
+  data.forEach((elt) => {
+    const temp = document.createElement("div");
+    temp.innerHTML = `
+      <figure data-id="${elt.id}" data-category-id="${elt.categoryId}">
         <i class="fa-solid fa-trash-can"></i>
-         <img src="${elt.imageUrl}" alt="${elt.title}" />
+        <img src="${elt.imageUrl}" alt="${elt.title}" />
       </figure>
     `;
-      const figure = temp.firstElementChild;
-      modaleGallery.appendChild(figure);
-    });
+    const figure = temp.firstElementChild;
+    modaleGallery.appendChild(figure);
+
+    figure
+      .querySelector(".fa-trash-can")
+      .addEventListener("click", () => deleteArticle(elt.id));
   });
 }
 
 function closeModale() {
-  const modaleWindow = document.querySelector("#modale");
+  const modaleWindow = document.querySelector("#modale-window");
   const modaleBg = document.querySelector("#modale-bg");
   modaleWindow.remove();
   modaleBg.remove();
   document.querySelector("body").style.overflow = "scroll";
+}
+
+async function deleteArticle(id) {
+  const token = localStorage.getItem("token");
+  try {
+    const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 204) {
+      const modalFigure = document
+        .querySelector(`#modale-gallery figure[data-id="${id}"]`)
+        .remove();
+
+      const mainFigure = document
+        .querySelector(`.gallery figure[data-id="${id}"]`)
+        .remove();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addArticle(id) {
+  const token = localStorage.getItem("token");
+  try {
+    const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 204) {
+      const modalFigure = document
+        .querySelector(`#modale-gallery figure[data-id="${id}"]`)
+        .remove();
+
+      const mainFigure = document
+        .querySelector(`.gallery figure[data-id="${id}"]`)
+        .remove();
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function showAddForm() {
+  const main = document.querySelector("#modale-main");
+  const form = document.querySelector("#modale-form");
+  const back = document.querySelector("#back");
+  main.style.right = "100%";
+  form.style.right = "0%";
 }
